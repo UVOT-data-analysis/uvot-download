f673edf94edf7bd9f9dd82fcfff51ae031bc6c9b
Lea Hagen <lhagen@stsci.edu>
2019-01-18 17:20:00 -0500

    use custom wget instead of browse_extract_wget.pl

--- uvot-download/query_heasarc.py
+++ uvot-download/query_heasarc.py
@@ -4,6 +4,9 @@ import stat
 import argparse
 import sys
 import glob
+import urllib.request
+
+import pdb
 
 def query_heasarc(input_obj, list_opt=None, search_radius=7.0):
     """
@@ -31,14 +34,30 @@ def query_heasarc(input_obj, list_opt=None, search_radius=7.0):
     
     for obj in obj_list:
 
-        #make new folders for each of the objects    
-        os.mkdir(obj)
-        os.chdir(obj)
-        
-        os.system('browse_extract_wget.pl table=swiftmastr position='
-                      + obj + ' radius='+str(search_radius)
-                      +' fields=obsid,start_time outfile=data.dat')
+        #make new folders for each of the objects
+        if not os.path.exists(obj):
+            os.mkdir(obj)
 
+        # command to generate HEASARC query
+        #cmd = 'browse_extract_wget.pl table=swiftmastr position=' \
+        #              + obj + ' radius='+str(search_radius) \
+        #              +' fields=obsid,start_time outfile=data.dat'
+        cmd = 'wget -O - -o /dev/null --no-check-certificate ' + "'" \
+              'https://heasarc.gsfc.nasa.gov/db-perl/W3Browse/w3query.pl?' + \
+              'tablehead='+urllib.request.quote('name=BATCHRETRIEVALCATALOG_2.0 swiftmastr') + \
+              '&Action=Query' + \
+              '&Coordinates='+urllib.request.quote("'Equatorial: R.A. Dec'") + \
+              '&Equinox=2000' + \
+              '&Radius='+str(search_radius) + \
+              '&NR=SIMBAD' + \
+              '&GIFsize=0' + \
+              '&Fields=&varon='+'&varon='.join(['obsid','start_time']) + \
+              '&Entry='+urllib.request.quote(obj) + \
+              '&displaymode=BatchDisplay' + "' > data.dat"
+              
+        os.system(cmd)
+
+        # read in the query output
         with open('data.dat', 'r') as fh:
             rows_list = fh.readlines()
 
@@ -48,7 +67,6 @@ def query_heasarc(input_obj, list_opt=None, search_radius=7.0):
             r = glob.glob(dir_path)
             for i in r:
                 os.remove(i)
-            os.chdir("..")
             continue
 
         #run browse_extract with all of the parameters needed to make data.dat
@@ -57,8 +75,10 @@ def query_heasarc(input_obj, list_opt=None, search_radius=7.0):
         #comments: comments out last line that lists number of observations returned for an object
         #skiprows: skips first two rows in data.dat that are just for formatting
 
+        #obslist = np.loadtxt('data.dat', dtype = 'str', delimiter = '|',
+        #                         comments = 'S', skiprows = 2, usecols = (1,2)).tolist()
         obslist = np.loadtxt('data.dat', dtype = 'str', delimiter = '|',
-                                 comments = 'S', skiprows = 2, usecols = (1,2)).tolist()
+                                 skiprows=3, comments='B', usecols = (1,2)).tolist()
         id_list = list()
 
         #if obslist is empty:
@@ -66,6 +86,11 @@ def query_heasarc(input_obj, list_opt=None, search_radius=7.0):
 
         # prefix for all of the wget commands
         wget_prefix = "wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/"
+
+        # make sure download script doesn't exist
+        if os.path.isfile('download.scr'):
+            os.remove('download.scr')
+            
 
         #consideration for a case where target that does not exist in heasarc
         if len(obslist[0]) > 2:
@@ -80,11 +105,11 @@ def query_heasarc(input_obj, list_opt=None, search_radius=7.0):
             wget_uvot = wget_prefix + start_month + '//' + obsid + "/uvot/"
             wget_auxil = wget_prefix + start_month + '//' + obsid + "/auxil/"
 
-            with open('download.scr', 'w+') as download_scr:
-                download_scr.write(wget_uvot)
-                download_scr.write("\n")
-                download_scr.write(wget_auxil)
-                download_scr.write("\n")
+            with open('download.scr', 'a') as download_scr:
+                download_scr.write(wget_uvot + '\n')
+                download_scr.write('mv '+obsid+'/uvot '+obj+'/ \n')
+                download_scr.write(wget_auxil + '\n')
+                download_scr.write('mv '+obsid+'/auxil '+obj+'/ \n')
             
         else:
             for i in range(len(obslist)):
@@ -99,11 +124,11 @@ def query_heasarc(input_obj, list_opt=None, search_radius=7.0):
                 wget_uvot = wget_prefix + start_month + '//' + obsid + "/uvot/"
                 wget_auxil = wget_prefix + start_month + '//' + obsid + "/auxil/"
 
-                with open('download.scr', 'w+') as download_scr:
-                    download_scr.write(wget_uvot)
-                    download_scr.write("\n")
-                    download_scr.write(wget_auxil)
-                    download_scr.write("\n")
+                with open('download.scr', 'a') as download_scr:
+                    download_scr.write(wget_uvot + '\n')
+                    download_scr.write('mv '+obsid+'/uvot '+obj+'/ \n')
+                    download_scr.write(wget_auxil + '\n')
+                    download_scr.write('mv '+obsid+'/auxil '+obj+'/ \n')
 
         #run the download script here and put the results in the directories created at the beginning of the list
 
