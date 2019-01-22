42b846bf8b375bd80f9b1ba11ab6350c970d4911
Lea Hagen <lhagen@stsci.edu>
2019-01-22 12:19:37 -0500

    Revert "move downloading to different script"
    This reverts commit 715e2021ef616e8139f4b7789b31f0651836a293.

--- uvot-download/query_heasarc.py
+++ uvot-download/query_heasarc.py
@@ -1,8 +1,11 @@
 import numpy as np
 import os
+import stat
 import argparse
 import sys
+import glob
 import urllib.request
+import subprocess
 
 import pdb
 
@@ -59,6 +62,89 @@ def query_heasarc(input_obj, list_opt=None, search_radius=7.0):
               
         os.system(cmd)
 
+        # read in the query output
+        with open('data.dat', 'r') as fh:
+            rows_list = fh.readlines()
+
+        if len(rows_list) == 1:
+            print("No observations of " + obj + " were found in HEASARC.")
+            dir_path = '*/*/*/' + gal
+            r = glob.glob(dir_path)
+            for i in r:
+                os.remove(i)
+            continue
+
+        #run browse_extract with all of the parameters needed to make data.dat
+
+        #important inputs for loadtxt:
+        #comments: comments out last line that lists number of observations returned for an object
+        #skiprows: skips first two rows in data.dat that are just for formatting
+
+        #obslist = np.loadtxt('data.dat', dtype = 'str', delimiter = '|',
+        #                         comments = 'S', skiprows = 2, usecols = (1,2)).tolist()
+        obslist = np.loadtxt('data.dat', dtype = 'str', delimiter = '|',
+                                 skiprows=3, comments='B', usecols = (1,2)).tolist()
+        id_list = list()
+
+        #if obslist is empty:
+        #    continue to next obj in obj_list, though if there's nothing that comes next, will it just end the program?
+
+        # prefix for all of the wget commands
+        wget_prefix = "wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/"
+
+        # make sure download script doesn't exist
+        if os.path.isfile('download.scr'):
+            os.remove('download.scr')
+            
+
+        #consideration for a case where target that does not exist in heasarc
+        if len(obslist[0]) > 2:
+            #print(obslist)
+            obsid = obslist[0]
+            starttime = obslist[1]
+            start_month = starttime[0:7]
+            start_month = start_month.replace('-','_')
+            id_list.append(obsid)
+
+        #    string addition to make wget commands for data download
+            wget_uvot = wget_prefix + start_month + '//' + obsid + "/uvot/"
+            wget_auxil = wget_prefix + start_month + '//' + obsid + "/auxil/"
+
+            with open('download.scr', 'a') as download_scr:
+                download_scr.write(wget_uvot + '\n')
+                download_scr.write(wget_auxil + '\n')
+                download_scr.write('mv '+obsid+' '+obj+'/ \n')
+            
+        else:
+            for i in range(len(obslist)):
+                #print(obslist[i])
+                [obsid, starttime] = obslist[i]
+        
+                start_month = starttime[0:7]
+                start_month = start_month.replace('-','_')
+                id_list.append(obsid)
+        
+            #    string addition to make wget commands for data download
+                wget_uvot = wget_prefix + start_month + '//' + obsid + "/uvot/"
+                wget_auxil = wget_prefix + start_month + '//' + obsid + "/auxil/"
+
+                with open('download.scr', 'a') as download_scr:
+                    download_scr.write(wget_uvot + '\n')
+                    download_scr.write(wget_auxil + '\n')
+                    download_scr.write('mv '+obsid+' '+obj+'/ \n')
+
+        #run the download script here and put the results in the directories created at the beginning of the list
+
+        #make download script executable
+        print("* running download script for "+obj)
+        os.system('sh download.scr')
+
+        #unzip all the downloaded data
+        print('* unzipping files')
+        for i in id_list:
+            gz_files = glob.glob(obj+'/'+i+'/**/*.gz', recursive=True)
+            for gz in gz_files:
+                subprocess.run('gunzip '+gz, shell=True)
 
 
 
