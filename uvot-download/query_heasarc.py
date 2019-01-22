333aabe324a671066f835b1787f3986f220edeb6
Lea Hagen <lhagen@stsci.edu>
2019-01-22 17:10:57 -0500

    option for any table parameters

--- uvot-download/query_heasarc.py
+++ uvot-download/query_heasarc.py
@@ -7,7 +7,7 @@ import urllib.request
 import pdb
 
 def query_heasarc(input_obj, list_opt=None, search_radius=7.0,
-                      create_folder=True):
+                      create_folder=True, table_params=['obsid','start_time']):
     """
     Find observations of a target in HEASARC
 
@@ -26,6 +26,12 @@ def query_heasarc(input_obj, list_opt=None, search_radius=7.0,
         choose whether to create a sub-folder for the object(s) or save the
         table into the current directory
 
+    table_params : list of strings (default=['obsid','start_time'])
+        List of parameters to extract from the observing information.  The
+        default params will be included in all queries.  More info (including
+        allowed params) here:
+        https://heasarc.gsfc.nasa.gov/W3Browse/swift/swiftmastr.html
+
     """
 
     #condition that handles either a list of entries from a file that needs to be loaded or a single object put into a list
@@ -34,6 +40,13 @@ def query_heasarc(input_obj, list_opt=None, search_radius=7.0,
         #print('expect a list and do list things')
     else:
         obj_list = [input_obj]
+
+    # ensure 'obsid' and 'start_time' are in table_params
+    if 'obsid' not in table_params:
+        table_params.append('obsid')
+    if 'start_time' not in table_params:
+        table_params.append('start_time')
+    
     
     for obj in obj_list:
 
@@ -61,7 +74,7 @@ def query_heasarc(input_obj, list_opt=None, search_radius=7.0,
               '&Radius='+str(search_radius) + \
               '&NR=SIMBAD' + \
               '&GIFsize=0' + \
-              '&Fields=&varon='+'&varon='.join(['obsid','start_time']) + \
+              '&Fields=&varon='+'&varon='.join(table_params) + \
               '&Entry='+urllib.request.quote(obj) + \
               '&displaymode=BatchDisplay' + \
               "' > " + output_file
